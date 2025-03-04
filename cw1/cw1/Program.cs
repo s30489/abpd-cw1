5ba2d5aed3f4007ed7a84bd45f8f488f6e7d084a
Adam Wyszyński <s30489@pjwstk.edu.pl>
2025-03-04 13:20:26 +0100

    Modified cw1

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Modyfikacja 1");
+
+static double srednia(int[] numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
+    sum /= numbers.Length;
+    return sum;
+}
+
+int[] arr = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine(srednia(arr));
